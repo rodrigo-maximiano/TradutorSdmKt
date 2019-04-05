1eea8ab1891c12b467e7332ff2648a400d860238
Rodrigo Maximiano <diguinho.max@gmail.com>
2019-04-05 20:03:44 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifsp/scl/tradutorsdmkt/Constantes.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.scl.tradutorsdmkt
+
+object Constantes {
+    // Constantes usadas para acesso ao WS de tradução
+    val URL_BASE = "https://od-api.oxforddictionaries.com/api/v1/"
+    val END_POINT = "entries"
+    val APP_ID_FIELD = "app_id"
+    val APP_ID_VALUE = "3df3eaaf" // Preencher com seu app_id
+    val APP_KEY_FIELD = "app_key"
+    val APP_KEY_VALUE = "d888b1986d31f8b044fb599cf27f5a81" // Preeencher com seu app_key
+}
